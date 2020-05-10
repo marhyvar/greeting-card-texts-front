8d30f976cf6090c8502c44e432dac4a28b4415dc
M. Hyvarinen <54714032+marhyvar@users.noreply.github.com>
2020-05-10 20:17:57 +0300

    Muokkaa palvelinta

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const cors = require('cors');
 const serveStatic = require("serve-static")
 const path = require('path');
 const app = express();
+app.use(cors());
 app.use(serveStatic(path.join(__dirname, 'dist')));
 const port = process.env.PORT || 80;
 app.listen(port);
